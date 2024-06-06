d39a9e8deb2356d69b120e4d1c27df62fa777f68
MischievousRay <148750307+MischievousRayIsDead@users.noreply.github.com>
2024-06-07 04:45:26 +0530

    Update pw_generator.py

--- pw_generator.py
+++ pw_generator.py
@@ -4,13 +4,13 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+num_of_letters = int(input("How many letters would you like in your password?\n")) 
+num_of_symbols = int(input(f"How many symbols would you like?\n"))
+num_of_numbers = int(input(f"How many numbers would you like?\n"))
 
-pw_letters = random.choices(letters, k=nr_letters)
-pw_symbols = random.choices(symbols, k=nr_symbols)
-pw_numbers = random.choices(numbers, k=nr_numbers)
+pw_letters = random.choices(letters, k=num_of_letters)
+pw_symbols = random.choices(symbols, k=num_of_symbols)
+pw_numbers = random.choices(numbers, k=num_of_numbers)
 
 pw_list = pw_letters + pw_symbols + pw_numbers
 
